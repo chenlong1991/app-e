fa33605add3efed357e2518412c1689fde53b9a4
chenlong1991 <914719011@qq.com>
2024-06-17 07:52:56 +0800

    win

--- src/components/WinClose.vue
+++ src/components/WinClose.vue
@@ -1,8 +1,11 @@
 <template>
-  <q-bar>
-    <q-btn dense flat icon="minimize" @click="minimize" />
-    <q-btn dense flat icon="crop_square" @click="maximize" />
-    <q-btn dense flat icon="close" @click="close" />
+  <q-bar class="win">
+    <div>设置</div>
+
+    <q-space />
+    <q-btn dense flat icon="remove" @click="minimize" class="btn" />
+    <q-btn dense flat icon="crop_square" @click="maximize" class="btn" />
+    <q-btn dense flat icon="close" @click="close" class="btn" />
   </q-bar>
 </template>
 
@@ -20,4 +23,11 @@ const close = () => {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.win {
+  -webkit-app-region: drag;
+}
+.btn {
+  -webkit-app-region: no-drag;
+}
+</style>
--- src/pages/TranslationSettings.vue
+++ src/pages/TranslationSettings.vue
@@ -1,11 +1,62 @@
 <template>
-  <q-page class="flex flex-center col">
-    <div>翻译设置</div>
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page class="fit justify-center col q-pt-md">
+    <div
+      class="q-mt-md full-width row no-wrap flex-center"
+      style="height: 40px"
+    >
+      <div class="q-mr-xs col-2 text-right self-center">划词翻译：</div>
+      <div class="col-8">
+        <q-input
+          outlined
+          placeholder="Placeholder"
+          dense
+          model-value=""
+          style="width: 300px"
+        >
+          <template v-slot:append>
+            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+    </div>
+    <div
+      class="q-mt-md full-width row no-wrap flex-center"
+      style="height: 40px"
+    >
+      <div class="q-mr-xs col-2 text-right self-center">截图翻译：</div>
+      <div class="col-8">
+        <q-input
+          outlined
+          placeholder="Placeholder"
+          dense
+          model-value=""
+          style="width: 300px"
+        >
+          <template v-slot:append>
+            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+    </div>
+    <div
+      class="q-mt-md full-width row no-wrap flex-center"
+      style="height: 40px"
+    >
+      <div class="q-mr-xs col-2 text-right self-center">输入翻译：</div>
+      <div class="col-8">
+        <q-input
+          outlined
+          placeholder="Placeholder"
+          dense
+          model-value=""
+          style="width: 300px"
+        >
+          <template v-slot:append>
+            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+    </div>
   </q-page>
 </template>
 
